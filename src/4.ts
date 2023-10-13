2cba9b96e222835374a418227095329751d7a8e0
Ivan Sheremeta <sheremetavanya2004@gmail.com>
2023-10-13 13:42:43 +0300

    task 4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,45 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+  abstract openDoor(key: Key): void;
+
+  constructor(key: Key) {
+    this.key = key;
+    this.door = false;
+  }
+
+  public comeIn(tenant: Person) {
+    if (this.door) {
+      this.tenants.push(tenant);
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +48,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
